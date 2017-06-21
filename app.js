c7720b9f99e15b868ab8e1f53618d9974a7b6cd0
niawkung <niawkung2@gmail.com>
2017-06-22 02:14:34 +0700

    add data to localStorage

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-    $(document).ready(function() {
-        $("#save").click(function() {
-            var fname = $("#fname").val();
-            var sub = $("#sub").val();
-            var mark = $("#mark").val();
+var tblRecord = [];
+$(document).ready(function() {
+    $("#save").click(function() {
+        var fname = $("#fname").val();
+        var sub = $("#sub").val();
+        var mark = $("#mark").val();
 
-            var tblrow = "<tr><td>" + 1 + "</td><td>" +
-                fname + "</td><td>" +
-                sub + "</td><td>" +
-                mark + "</td></tr>";
 
-            $("tbody").append(tblrow);
-            $("#fname").val("");
-            $("#sub").val("");
-            $("#mark").val("");
-        });
+        var tblrow = "<tr><td>" + 1 + "</td><td>" +
+            fname + "</td><td>" +
+            sub + "</td><td>" +
+            mark + "</td></tr>";
+
+
+        var tblObj = {};
+        tblObj.fname = fname;
+        tblObj.sub = sub;
+        tblObj.mark = mark;
+        tblRecord.push(tblObj);
+        console.log(tblRecord);
+        localStorage.tblData = JSON.stringify(tblRecord);
+        $("tbody").append(tblrow);
+        $("#fname").val("");
+        $("#sub").val("");
+        $("#mark").val("");
     });
+});
